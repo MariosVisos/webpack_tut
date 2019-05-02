d34eda4831c655dd7f91d6c6ae7f96d24497b6e0
marios_visos <visos.marios@gmail.com>
2019-05-02 16:40:21 +0200

    First commit

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+// npx webpack --config webpack.config.js to run the build using this config (--config optional)
+// npm run build
+// Custom parameters can be passed to webpack by adding two dashes between the npm run build command and your parameters, e.g. npm run build -- --colors.
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist')
+    }
+};
