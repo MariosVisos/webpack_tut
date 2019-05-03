4b0d2b4cf6ddb903f8d07917eea6f7828b82a31b
marios_visos <visos.marios@gmail.com>
2019-05-03 10:59:18 +0200

    Prevent duplication

--- webpack.config.js
+++ webpack.config.js
@@ -9,10 +9,15 @@ const webpack = require('webpack');
 // Now adjust the config. We'll be adding our src/print.js as a new entry point (print) and we'll change the output as well, so that it will dynamically 
 // generate bundle names, based on the entry point names:
 
-//Before we do a build, you should know that the HtmlWebpackPlugin by default will generate its own index.html file, even though we already have one in
+// Before we do a build, you should know that the HtmlWebpackPlugin by default will generate its own index.html file, even though we already have one in
 // the dist/ folder. This means that it will replace our index.html file with a newly generated one.
 
 // You can use the CLI to modify the webpack-dev-server configuration with the following command: webpack-dev-server --hotOnly.
+// useful plugins and loaders provided by the community for splitting code:
+
+// mini-css-extract-plugin: Useful for splitting CSS out from the main application.
+// bundle-loader: Used to split code and lazy load the resulting bundles.
+// promise-loader: Similar to the bundle-loader but uses promises.
 
 module.exports = {
     mode: 'development',
@@ -44,6 +49,19 @@ module.exports = {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
         publicPath: '/'
+    },
+    optimization: {
+        splitChunks: {
+            chunks: 'all'
+        }
     }
 };
 
+// When using multiple entry points: As mentioned there are some pitfalls to this approach:
+// If there are any duplicated modules between entry chunks they will be included in both bundles.
+// It isn't as flexible and can't be used to dynamically split code with the core application logic.
+// The first of these two points is definitely an issue for our example, as lodash is also imported
+//  within ./src/index.js and will thus be duplicated in both bundles. Let's remove this duplication by using the SplitChunksPlugin.
+
+//The SplitChunksPlugin allows us to extract common dependencies into an existing entry chunk or an entirely new chunk. Let's use this
+//  to de-duplicate the lodash dependency from the previous example:
